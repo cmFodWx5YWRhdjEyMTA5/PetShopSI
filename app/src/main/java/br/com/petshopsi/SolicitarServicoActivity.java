0c2dab1d02e417e65376c0957afae3e62d2d90c5
suellencarvalho <suellenncontato@gmail.com>
2020-05-19 17:22:14 -0300

    Atualizações no codigo da tela de solicitar serviço, inclusao de validação no check box de transporte.

--- app/src/main/java/br/com/petshopsi/SolicitarServicoActivity.java
+++ app/src/main/java/br/com/petshopsi/SolicitarServicoActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,23 +16,90 @@ import java.util.List;
 public class SolicitarServicoActivity extends AppCompatActivity {
 
     Spinner spinner;
+    private CheckBox checkBoxBanho;
+    private CheckBox checkBoxTosa;
+    private CheckBox checkBoxTransporte;
+    private EditText datasolicitacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitar_servico);
 
+        // cast
         spinner = (Spinner) findViewById(R.id.spinner);
+        checkBoxBanho = (CheckBox) findViewById(R.id.checkBanho);
+        checkBoxTosa = (CheckBox) findViewById(R.id.checkTosa);
+        checkBoxTransporte = (CheckBox) findViewById(R.id.checkTransporte);
+        datasolicitacao = (EditText) findViewById(R.id.data_solicitacao);
+
+
 
         List<String> list = new ArrayList<String>();
         list.add("Busca");
         list.add("Entrega");
         list.add("Busca e Entrega");
+
+
+
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
 
 
+
+        checkBoxBanho.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+                    Toast.makeText(SolicitarServicoActivity.this, "Marcado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        checkBoxTosa.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+                    Toast.makeText(SolicitarServicoActivity.this, "Marcado", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SolicitarServicoActivity.this, "Não marcado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        //Validar se o checkbox de transporte esta selecionado
+        //spinner ser apresentado desabilitado ao carregar a tela
+        spinner.setEnabled(false);
+
+        checkBoxTransporte.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+                    spinner.setEnabled(true);
+                    Toast.makeText(SolicitarServicoActivity.this, "Marcado", Toast.LENGTH_SHORT).show();
+                } else {
+                    spinner.setEnabled(false);
+                    Toast.makeText(SolicitarServicoActivity.this, "Não marcado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
+
+
 }
